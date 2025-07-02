94a78d5d846a8b4b3589130d4187f937030da002
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-07-02 21:39:14 +0700

    add charge method & fix bug cancel method

--- app/Services/Midtrans/Transaction.php
+++ app/Services/Midtrans/Transaction.php
@@ -2,10 +2,20 @@
 
 namespace App\Services\Midtrans;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
 
 class Transaction
 {
+    public static function charge($charge_param)
+    {
+        return Http::withBasicAuth(config("midtrans.server_key"), "")
+            ->withHeaders([
+                'X-Append-Notification' => config("midtrans.demo_url") . "callback-notification"
+            ])
+            ->post(config("midtrans.sandbox_base_url") . "v2/charge", $charge_param);
+    }
+
     public static function status($id)
     {
         return Http::withBasicAuth(config('midtrans.server_key'), '')->get(config("midtrans.sandbox_base_url") . "v2/" . $id . "/status");
@@ -13,6 +23,17 @@ class Transaction
 
     public static function cancel($id)
     {
-        return Http::withBasicAuth(config('midtrans.server_key'), '')->post(config("midtrans.sandbox_base_url") . "v2/" . $id . "/cancel");
+        $client = new Client();
+
+        return $client->post(
+            config("midtrans.sandbox_base_url") . "v2/" . $id . "/cancel",
+            [
+                'headers' => [
+                    'accept' => 'application/json',
+                    'authorization' => 'Basic ' . base64_encode(config('midtrans.server_key') . ':'),
+                    'X-Append-Notification' => config("midtrans.demo_url") . "callback-notification"
+                ]
+            ]
+        );
     }
 }
